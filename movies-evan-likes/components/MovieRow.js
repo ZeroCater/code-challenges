3634750df3343d26cc948e8b9fa20db069b61c90
mrezo <balarezo.m@gmail.com>
2016-06-04 14:52:14 -0700

    Adds ternary operator to output boolean as string for wonBestPicture

--- movies-evan-likes/components/MovieRow.js
+++ movies-evan-likes/components/MovieRow.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+// JSX does not render boolean values from data set. Added ternary statement
+// to render boolean as string.
 const MovieRow = (props) => (
   <tr>
     <td>{props.data.title}</td>
     <td>{props.data.year}</td>
-    <td>{props.data.wonBestPicture}</td>
+    <td>{props.data.wonBestPicture ? 'true' : 'false'}</td>
     <td>{props.data.genre}</td>
   </tr>
 );
