183ac0cf4a018b8dc7540ee61797ca2f292451f8
mrezo <balarezo.m@gmail.com>
2016-06-04 14:19:20 -0700

    Refactors component. <tbody> displays the 'rows'. The 'rows' variable maps over movie data and returns each movie as a prop to the MovieRow component.

--- movies-evan-likes/components/Results.js
+++ movies-evan-likes/components/Results.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import MovieRow from './MovieRow';
 
 export default class Results extends Component {
   render() {
     const { data } = this.props;
+    let rows = this.props.data.map(movie => {
+      return <MovieRow data={movie} />;
+    });
 
     return (
       <table>
-        {this.props.data.map(movie =>
-          <tr key={movie.title}> {movie.title} </tr>
-        )}
+        <tbody>{rows}</tbody>
       </table>
     );
   }
