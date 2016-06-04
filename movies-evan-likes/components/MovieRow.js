c5e5ccc02c1625d0e4eb149f86ae777a2affbdf4
mrezo <balarezo.m@gmail.com>
2016-06-04 14:56:11 -0700

    Changes boolean output for wonbestPicture to 'yes' or 'no'. Make it easier for user to understand.

--- movies-evan-likes/components/MovieRow.js
+++ movies-evan-likes/components/MovieRow.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-// JSX does not render boolean values from data set. Added ternary statement
-// to render boolean as string.
+// JSX does not render boolean values from data set. Added ternary operator
+// to render boolean as string that user can understand. Does not modify
+// original data.
+
 const MovieRow = (props) => (
   <tr>
     <td>{props.data.title}</td>
     <td>{props.data.year}</td>
-    <td>{props.data.wonBestPicture ? 'true' : 'false'}</td>
+    <td>{props.data.wonBestPicture ? 'yes' : 'no'}</td>
     <td>{props.data.genre}</td>
   </tr>
 );
