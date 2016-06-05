923c8222e0ccb4b7eddb4ca5021ecff98a3a28c7
mrezo <balarezo.m@gmail.com>
2016-06-04 18:47:15 -0700

    Imports GenreFilter component and adds it to render function

--- movies-evan-likes/components/Filters.js
+++ movies-evan-likes/components/Filters.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import GenreFilter from './GenreFilter';
 
 export default class Filters extends Component {
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <fieldset>
-          Put the filter controls here
-          <button>Filter</button>
-        </fieldset>
-      </form>
-    );
+  constructor(props) {
+    super();
   }
 
   handleSubmit(e) {
     e.preventDefault();
   }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <fieldset>
+            Put the filter controls here
+            <GenreFilter />
+            <button>Filter</button>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
+
 }
