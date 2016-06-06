59aee38fdd2f84e699007a981d9ba28b9cdabca0
mrezo <balarezo.m@gmail.com>
2016-06-06 09:55:28 -0700

    Adds handleUserSubmit function in filter. Creates a state for the filter and passes it down as props to other filter components

--- movies-evan-likes/components/Filters.js
+++ movies-evan-likes/components/Filters.js
@@ -6,10 +6,24 @@ import SearchBar from './SearchBar.js';
 export default class Filters extends Component {
   constructor(props) {
     super();
+    this.state = {
+      searchText: '',
+      bestPicture: false,
+      genre: 'null',
+    };
+  }
+
+  handleUserInput(searchTextInput, bestPictureInput, genreInput) {
+    this.setState({
+      searchText: searchTextInput,
+      bestPicture: bestPictureInput,
+      genre: genreInput,
+    });
   }
 
   handleSubmit(e) {
     e.preventDefault();
+    this.props.setFilteredResults(this.state.searchText, this.state.bestPicture, this.state.genre);
   }
 
   render() {
@@ -18,18 +32,21 @@ export default class Filters extends Component {
         <form onSubmit={this.handleSubmit}>
           <fieldset>
             <GenreFilter
-              searchText={this.props.searchText}
-              bestPicture={this.props.bestPicture}
-              genre={this.props.genre}
-              onUserInput={this.props.onUserInput}
+              searchText={this.state.searchText}
+              bestPicture={this.state.bestPicture}
+              genre={this.state.genre}
+              onUserInput={this.handleUserInput}
               data={this.props.data}
             />
-            <SearchBar searchText={this.props.searchText} onUserInput={this.props.onUserInput} />
+            <SearchBar
+              searchText={this.state.searchText}
+              onUserInput={this.state.handleUserInput}
+            />
             <WonBestPicture
-              searchText={this.props.searchText}
-              bestPicture={this.props.bestPicture}
-              genre={this.props.genre}
-              onUserInput={this.props.onUserInput}
+              searchText={this.state.searchText}
+              bestPicture={this.state.bestPicture}
+              genre={this.state.genre}
+              onUserInput={this.handleUserInput}
             />
             <button>Filter</button>
           </fieldset>
