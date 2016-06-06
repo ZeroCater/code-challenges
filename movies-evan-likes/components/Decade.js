0ca4959bdca2783be6e38438272e3422d93b71bc
mrezo <balarezo.m@gmail.com>
2016-06-06 15:08:13 -0700

    Adds boilerplate code from Genre

---
+++ movies-evan-likes/components/Decade.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+export default class GenreFilter extends Component {
+  constructor(props) {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    this.props.onUserInput(this.props.searchText, this.props.bestPicture, this.refs.genreInput.value);
+  }
+
+  render() {
+    let options = [];
+
+    this.props.data.forEach(movie => {
+      if (options.indexOf(movie.genre) === -1) {
+        options.push(movie.genre);
+      }
+    });
+
+    return (
+      <div className="genre-selector">
+        <select ref="genreInput" onChange={this.handleChange}>
+          <option key="all" value="null">-</option>
+          {options.map(genre => <option key={genre} value={genre}>{genre}</option>)}
+        </select>
+      </div>
+    );
+  }
+}
