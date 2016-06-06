10b6d1a2fcf8451df28aa94752f63dcbbec8b4b7
mrezo <balarezo.m@gmail.com>
2016-06-06 09:56:32 -0700

    Refactors logic to display results on render if state remains unchanged

--- movies-evan-likes/components/Results.js
+++ movies-evan-likes/components/Results.js
@@ -8,25 +8,29 @@ export default class Results extends Component {
 
   render() {
     let rows = [];
-    this.props.data.forEach(movie => {
 
-      if ((this.props.genre !== 'null') && (movie.genre !== this.props.genre)) {
-        return;
-      }
-      
-      if (!movie.wonBestPicture && this.props.bestPicture) {
-        return;
-      }
-      
-      if (movie.title.indexOf(this.props.searchText) === -1) {
-        return;
-      }
+    // create conditional that checks if any of the intial values are different, if so, proceed with filter, if not, render all
 
-
-      if (rows.indexOf(movie) === -1) {
+    if ((this.props.genre === 'null') && !this.props.bestpicture && (this.props.searchText.length < 1)) {
+      this.props.data.forEach(movie => {
         rows.push(movie);
-      }
-    });
+      });
+    }
+
+      // if ((this.props.genre !== 'null') && (movie.genre !== this.props.genre)) {
+      //   return;
+      // }
+      
+      // if (!movie.wonBestPicture && this.props.bestPicture) {
+      //   return;
+      // }
+      
+      // if (movie.title.indexOf(this.props.searchText) === -1) {
+      //   return;
+      // }
+
+
+      // if (rows.indexOf(movie) === -1) {
 
     return (
       <table>
