0d1d1d9e838affb9566f31814895a9a28ca2da93
mrezo <balarezo.m@gmail.com>
2016-06-06 12:33:09 -0700

    Restructures logic to keep track of how many times a movie has been applied to filters. Keeps track of count and filtered Movies in seaprate variables

--- movies-evan-likes/components/Results.js
+++ movies-evan-likes/components/Results.js
@@ -8,10 +8,18 @@ export default class Results extends Component {
 
   render() {
     let rows = [];
+    let filteredMovies = [];
+    let counter = 0;
 
+    // Get all the movie titles since they are unique and store them as a key with value 0
+    let movieTitles = {};
+    this.props.data.forEach(movie => {
+      movieTitles[movie.title] = 0;
+    });
+    
     // If the filtered state is unchanged, render all the movies
     // Otherwise, compare the movie to the props values and push to rows array
-    
+
     if ((this.props.genre === 'null') && !this.props.bestpicture && (this.props.searchText.length < 1)) {
       this.props.data.forEach(movie => {
         rows.push(movie);
@@ -19,16 +27,62 @@ export default class Results extends Component {
     } else {
       this.props.data.forEach(movie => {
         if (movie.genre === this.props.genre) {
-          rows.push(movie);
+          movieTitles[movie.title] += 1;
+          console.log(movieTitles);
         }
-        if (movie.title.indexOf(this.props.searchText) !== -1) {
-          rows.push(movie);
-        }
+      });
+      this.props.data.forEach(movie => {
         if (movie.wonBestPicture === this.props.bestPicture) {
-          rows.push(movie);
+          movieTitles[movie.title] += 1;
+          console.log(movieTitles);
+        }
+      });
+      this.props.data.forEach(movie => {
+        if (movie.title === this.props.searchText) {
+          movieTitles[movie.title] += 1;
+          console.log(movieTitles);
         }
       });
     }
+
+    // Find the movies with the highest values. These are the movies with the most filtered matches.
+
+    //Set the high count value
+    for (var key in movieTitles) {
+      if (movieTitles[key] > counter) {
+        counter = movieTitles[key];
+      }
+    }
+    console.log('This is my counter', counter);
+
+    // Loop thorugh the movieTitles array and push them to a the filteredMovies array
+
+    for (var key in movieTitles) {
+      if (movieTitles[key] === counter) {
+        filteredMovies.push(key);
+      }
+    }
+    console.log(filteredMovies);
+
+    // Loop through the data checking against the filtered movies array and push the movie objects to rows array
+    for (var i = 0; i < this.props.data.length; i++) {
+      for (var j = 0; j < filteredMovies.length; j++) {
+        if (this.props.data[i].title === filteredMovies[j]) {
+          rows.push(this.props.data[i]);
+        }
+      }
+    }
+
+    // this.props.data.forEach(movie => {
+    //   for(var i = 0; i < movieTitles)
+    //   if(movie.title.indexOf())
+    // });
+        // if (movie.title.indexOf(this.props.searchText) !== -1) {
+        //   rows.push(movie);
+        // }
+        // if (movie.wonBestPicture === this.props.bestPicture) {
+        //   rows.push(movie);
+        // }
     // Go through each movie and check if any of the filters apply before adding to the rows array
 
 
