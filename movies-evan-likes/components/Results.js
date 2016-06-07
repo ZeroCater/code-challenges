c5116ed732a4fc06af7eba2a2400610c6d2b2b30
mrezo <balarezo.m@gmail.com>
2016-06-06 17:45:43 -0700

    Removes redundant forEach methods in else block. Removes unecessary if statement for checking if prop values match movie values. Type coerces this.props.decade to Number.

--- movies-evan-likes/components/Results.js
+++ movies-evan-likes/components/Results.js
@@ -11,39 +11,32 @@ export default class Results extends Component {
     let filteredMovies = [];
     let counter = 0;
 
-    // Get all the movie titles since they are unique and store them as the key with value 0
+    // Get all the movie titles and store them as a key with value 0
     let movieTitles = {};
     this.props.data.forEach(movie => {
       movieTitles[movie.title] = 0;
     });
-    
-    // If the filtered state is unchanged, render all the movies
-    // Otherwise, compare the movie to the props values and push to rows array
-    // =======================================================================
-    if ((this.props.genre === 'null') && !this.props.bestPicture && (this.props.searchText.length < 1)) {
-      this.props.data.forEach(movie => {
-        rows.push(movie);
-      });
-    } else {
-      this.props.data.forEach(movie => {
-        if (movie.genre === this.props.genre) {
-          movieTitles[movie.title] += 1;
-        }
-      });
-      this.props.data.forEach(movie => {
-        if (movie.wonBestPicture === this.props.bestPicture) {
-          movieTitles[movie.title] += 1;
-        }
-      });
-      this.props.data.forEach(movie => {
-        if (movie.title === this.props.searchText) {
-          movieTitles[movie.title] += 1;
-        }
-      });
-    }
 
-    // Find the movies with the highest values. These are the movies with the most filtered matches.
-    // =============================================================================================
+    // Filter check to see if the a movie matches the current criteria
+    this.props.data.forEach(movie => {
+      if (movie.genre === this.props.genre) {
+        movieTitles[movie.title] += 1;
+      }
+      if (movie.wonBestPicture === this.props.bestPicture) {
+        movieTitles[movie.title] += 1;
+      }
+      if (movie.title === this.props.searchText) {
+        movieTitles[movie.title] += 1;
+      }
+      if ((Math.floor(movie.year / 10) * 10) === Number(this.props.decade)) {
+        movieTitles[movie.title] += 1;
+      }
+    });
+
+
+    // Initially all movies in movieTitles have a value of 0 so they all get rendered.
+    // After changes are made to the state we push the highest valued keys to the rows array for rendering
+    // ===================================================================================================
 
     //Set the high count value
     for (var key in movieTitles) {
@@ -52,7 +45,7 @@ export default class Results extends Component {
       }
     }
 
-    // Loop thorugh the movieTitles array and push them to a the filteredMovies array
+    // Loop thorugh the movieTitles object and push them to a the filteredMovies array
     for (var key in movieTitles) {
       if (movieTitles[key] === counter) {
         filteredMovies.push(key);
@@ -78,7 +71,7 @@ export default class Results extends Component {
             <th>Genre</th>
           </tr>
         </tbody>
-        <tbody>{rows.map(movie => <MovieRow key={movie.title} data={movie} />)}</tbody>
+        <tbody>{rows.map((movie, i) => <MovieRow key={i} data={movie} />)}</tbody>
       </table>
     );
   }
