760154c7567f3b118b054f7094e50f0ccd869556
mrezo <balarezo.m@gmail.com>
2016-06-10 09:22:01 -0700

    Starts to re-arrange logic to have work with filteredState object

--- movies-evan-likes/components/Results.js
+++ movies-evan-likes/components/Results.js
@@ -8,29 +8,27 @@ export default class Results extends Component {
 
   render() {
     let rows = [];
-    let filteredMovies = [];
-    let counter = 0;
+    // let filteredMovies = [];
+    // let counter = 0;
 
     // Get all the movie titles and store them as a key with value 0
-    let movieTitles = {};
-    this.props.data.forEach(movie => {
-      movieTitles[movie.title] = 0;
-    });
+    // let movieTitles = {};
+    // this.props.data.forEach(movie => {
+    //   movieTitles[movie.title] = 0;
+    // });
 
     // Filter check to see if the a movie matches the current criteria
+
     this.props.data.forEach(movie => {
-      if (movie.genre === this.props.genre) {
-        movieTitles[movie.title] += 1;
+      if ( (this.props.searchText === '') && (this.props.bestPicture === false) && (this.props.genre === '') && (this.props.decade === '') ) {
+        rows.push(movie)
       }
-      if (movie.wonBestPicture && this.props.bestPicture) {
-        movieTitles[movie.title] += 1;
-      }
-      if (movie.title.indexOf(this.props.searchText) !== -1) {
-        movieTitles[movie.title] += 1;
-      }
-      if ((Math.floor(movie.year / 10) * 10) === Number(this.props.decade)) {
-        movieTitles[movie.title] += 1;
-      }
+
+      if ( (movie.genre === this.props.genre) && (movie.wonBestPicture && this.props.bestPicture) && (movie.title.indexOf(this.props.searchText) !== -1) && (Math.floor(movie.year / 10) * 10) === Number(this.props.decade) ) {
+        rows.push(movie);
+      } 
+     
+      
     });
 
 
@@ -40,27 +38,27 @@ export default class Results extends Component {
     // ===================================================================================================
 
     //Set the high count value
-    for (var key in movieTitles) {
-      if (movieTitles[key] > counter) {
-        counter = movieTitles[key];
-      }
-    }
+    // for (var key in movieTitles) {
+    //   if (movieTitles[key] > counter) {
+    //     counter = movieTitles[key];
+    //   }
+    // }
 
-    // Loop thorugh the movieTitles object and push them to a the filteredMovies array
-    for (var key in movieTitles) {
-      if (movieTitles[key] === counter) {
-        filteredMovies.push(key);
-      }
-    }
+    // // Loop thorugh the movieTitles object and push them to a the filteredMovies array
+    // for (var key in movieTitles) {
+    //   if (movieTitles[key] === counter) {
+    //     filteredMovies.push(key);
+    //   }
+    // }
 
-    // Loop through the data checking against the filtered movies array and push the movie objects to rows array
-    for (var i = 0; i < this.props.data.length; i++) {
-      for (var j = 0; j < filteredMovies.length; j++) {
-        if (this.props.data[i].title === filteredMovies[j]) {
-          rows.push(this.props.data[i]);
-        }
-      }
-    }
+    // // Loop through the data checking against the filtered movies array and push the movie objects to rows array
+    // for (var i = 0; i < this.props.data.length; i++) {
+    //   for (var j = 0; j < filteredMovies.length; j++) {
+    //     if (this.props.data[i].title === filteredMovies[j]) {
+    //       rows.push(this.props.data[i]);
+    //     }
+    //   }
+    // }
 
     return (
       <table>
