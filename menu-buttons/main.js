be18625673da51f45b72adee0a94f8b6dbcbe0da
mrezo <balarezo.m@gmail.com>
2016-06-15 15:19:47 -0500

    Adds else block to lorem function to remove the 'is-active' class. Adds window.onclick function to remove 'is-active' class

--- menu-buttons/main.js
+++ menu-buttons/main.js
@@ -4,14 +4,29 @@ var linkList = document.getElementsByClassName("link");
 for (var i = 0; i < linkList.length; i++) {
   linkList[i].addEventListener('click', lorem);
 }
+
 // Toggles the dropdown on click event
 function lorem (event) {
   var el = event.target;
 
 for (var i = 0; i < linkList.length; i++) {
-    if (linkList[i].contains(el)){
+    if (linkList[i].contains(el)) {
       linkList[i].classList.toggle('is-active')
+    } else {
+      linkList[i].classList.remove('is-active');
     }
   }
 }
+
+// Removes the dropdown menu by clicking anywhere on the window
+window.onclick = function(event) {
+  var el = event.target;
+  if (!el.matches('a')) {
+    for (var i = 0; i < linkList.length; i++) {
+      if (linkList[i].classList.contains('is-active')){
+        linkList[i].classList.remove('is-active');
+      }
+    }
+  }
+};
 
