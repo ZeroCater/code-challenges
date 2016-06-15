04451238b62dbae396b1322046be1551699d5efb
mrezo <balarezo.m@gmail.com>
2016-06-15 11:32:55 -0500

    Adds function that loops over the linkList variable and toggles the dropdown menu for the clicked element

--- menu-buttons/main.js
+++ menu-buttons/main.js
@@ -4,4 +4,14 @@ var linkList = document.getElementsByClassName("link");
 for (var i = 0; i < linkList.length; i++) {
   linkList[i].addEventListener('click', lorem);
 }
+// Toggles the dropdown on click event
+function lorem (event) {
+  var el = event.target;
+
+for (var i = 0; i < linkList.length; i++) {
+    if (linkList[i].contains(el)){
+      linkList[i].classList.toggle('is-active')
+    }
+  }
+}
 
