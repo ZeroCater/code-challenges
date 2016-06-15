79772e2ce571d7dd57112ddec60a300e9ecf51d4
mrezo <balarezo.m@gmail.com>
2016-06-15 15:25:37 -0500

    Fixes spacing

--- menu-buttons/main.js
+++ menu-buttons/main.js
@@ -9,18 +9,19 @@ for (var i = 0; i < linkList.length; i++) {
 function lorem (event) {
   var el = event.target;
 
-for (var i = 0; i < linkList.length; i++) {
-    if (linkList[i].contains(el)) {
-      linkList[i].classList.toggle('is-active')
-    } else {
-      linkList[i].classList.remove('is-active');
+  for (var i = 0; i < linkList.length; i++) {
+      if (linkList[i].contains(el)) {
+        linkList[i].classList.toggle('is-active')
+      } else {
+        linkList[i].classList.remove('is-active');
+      }
     }
-  }
 }
 
 // Removes the dropdown menu by clicking anywhere on the window
 window.onclick = function(event) {
   var el = event.target;
+  
   if (!el.matches('a')) {
     for (var i = 0; i < linkList.length; i++) {
       if (linkList[i].classList.contains('is-active')){
