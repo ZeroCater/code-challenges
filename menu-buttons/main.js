227a1eed1a3dd0bc0fd4e00dee31180dc4be80ca
mrezo <balarezo.m@gmail.com>
2016-06-15 11:31:42 -0500

    Adds click event listener to all elements with class of link

--- menu-buttons/main.js
+++ menu-buttons/main.js
@@ -1,7 +1,7 @@
-function lorem () {
-  console.log('lorem executed');
+var linkList = document.getElementsByClassName("link");
+
+// Adds click listener to all elements with class of 'link'
+for (var i = 0; i < linkList.length; i++) {
+  linkList[i].addEventListener('click', lorem);
 }
 
-function ipsum () {
-  console.log('ipsum executed');
-}
